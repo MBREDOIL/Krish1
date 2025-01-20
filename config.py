8e3b676c925a2bc6d9a4a48cfe5a55fe6a46d0c9
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2025-01-20 22:43:03 +0530

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-BOT_NAME = "Luminatbot"
+BOT_NAME  = environ.get("BOT_NAME", "")
 API_ID    = 21567814
 API_HASH  = "cd7dc5431d449fd795683c550d7bfb7e"
-BOT_TOKEN = "7662813243:AAEA3yIU_BZTCHR-lEXRFBelPfDdubYuRFA"
-#MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
-MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+MONGO_URI = environ.get("MONGO_URI", "")
